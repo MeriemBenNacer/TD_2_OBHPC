616602cbdf354163c1d0bccf87a8f0049082112e
Meriem Bennacer <meriem.ben-nacer@ens.uvsq.fr>
2022-11-14 16:13:55 +0100

    dossier dotprod

---
+++ dotprod/kernels.c
@@ -0,0 +1,45 @@
+//
+#if defined(__INTEL_COMPILER)
+#include <mkl.h>
+#else
+#include <cblas.h>
+#endif
+
+//
+#include "types.h"
+
+//Baseline - naive implementation
+f64 dotprod_base(f64 *restrict a, f64 *restrict b, u64 n)
+{
+  double d = 0.0;
+  
+  for (u64 i = 0; i < n; i++)
+    d += a[i] * b[i];
+
+  return d;
+}
+f64 dotprod_UNROLL8(f64 *restrict a, f64 *restrict b, u64 n)
+{
+#define UNROLL8 8
+  
+      double d = 0.0;
+  
+       //Unroll 8 times
+	  for (u64 i = 0; i < (n - (n & (UNROLL8 - 1))); i += UNROLL8){
+      d += a[i] * b[i];
+      d += a[i+1] * b[i+1];
+      d += a[i+2] * b[i+2];
+      d += a[i+3] * b[i+3];
+      d += a[i+4] * b[i+4];
+      d += a[i+5] * b[i+5];
+      d += a[i+6] * b[i+6];
+      d += a[i+7] * b[i+7];
+      
+         }
+	  //Manage the leftovers
+	  for (u64 j = (n - (n & 7)); j < n; j++)
+	    d += a[j] * b[j];
+	 
+ return d;
+}
+
